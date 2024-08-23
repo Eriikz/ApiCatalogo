5f25f39139c83b505e0373001170c3bdafdcc28b
Erik Fernandes <erik.fernandess@hotmail.com>
2024-08-23 11:35:47 -0300

    feat: novidades

--- APICatalogo/obj/Debug/net6.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net6.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+105c81bbd156676bc4cf6bf581e8423bbbc69236")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
